b8bd4de231c7bc78ca7d32d7827d2f637bc5ad57
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-06-26 13:17:40 +0200

    change google link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,24 @@
-import React from "react"
+import React from "react";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <img src={`../images/${props.img}`} className="cardImage" alt="test" />
-            <div className="cardStats">
-            <h2>📍 {props.location}    <a className="map">{props.googleMapsUrl}</a> </h2>
-                <h1>{props.title}</h1>
-                <p><span className="btn">{props.startDate} _ {props.endDate}</span></p>
-                <p>{props.description}</p>
-            
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="card">
+      <img src={`../images/${props.img}`} className="cardImage" alt="test" />
+      <div className="cardStats">
+        <h2>
+          📍 {props.location}{" "}
+          <a className="map" href={props.googleMapsUrl} target="_blank">
+            See on Google
+          </a>
+        </h2>
+        <h1>{props.title}</h1>
+        <p>
+          <span className="btn">
+            {props.startDate} _ {props.endDate}
+          </span>
+        </p>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
 }
